c0e148382d65cffe4d19386808e5fa0906ef51fe
Alex Gerus <alexander.gerus@grse.de>
2023-01-08 22:42:39 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Alexander Gerus on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
